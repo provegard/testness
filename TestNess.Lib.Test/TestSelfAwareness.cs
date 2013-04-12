3b7d59180f29a86393a625357bed553132b1cd91
Per Rovegard <per@rovegard.se>
2013-04-12 15:17:23 +0200

    Added self-awareness test case
    Requires a score of 0, but made Explicit for now since TestNess doesn't
    handle LINQ in test cases or mock frameworks yet.
    
    Fixes #26

---
+++ TestNess.Lib.Test/TestSelfAwareness.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (C) 2011-2013 Per Rovegård, http://rovegard.com
+// This file is subject to the terms and conditions of the MIT license. See the file 'LICENSE',
+// which is part of this source code package, or http://per.mit-license.org/2011.
+using System;
+using System.Linq;
+using NUnit.Framework;
+using TestNess.Lib.Analysis;
+using TestNess.Lib.Rule;
+
+namespace TestNess.Lib.Test
+{
+    [TestFixture]
+    public class TestSelfAwareness
+    {
+        [Test, Explicit]
+        public void MyOwnScoreShouldBe0()
+        {
+            var assemblyFile = new Uri(GetType().Assembly.CodeBase).LocalPath;
+            var repo = TestCases.LoadFromFile(assemblyFile);
+            var rules = new Rules(typeof(IRule).Assembly);
+            var results = AnalysisResults.Create(repo, rules, new ViolationScorer());
+            var score = results.Applications.Sum(a => a.Score);
+            foreach (var v in results.Applications.SelectMany(a => a.Violations))
+            {
+                Console.WriteLine(v);
+            }
+            Assert.AreEqual(0m, score);
+        }
+    }
+}
