1e7ad5fff8e5d6c4054911003682184fc3e55d45
Per Rovegard <per@rovegard.se>
2014-07-04 16:07:31 +0200

    Added an InstructionGraph helper.

---
+++ TestNess.Lib.Test/Cil/InstructionGraphTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TestNess.Lib.Cil;
+
+namespace TestNess.Lib.Test.Cil
+{
+    [TestFixture]
+    public class InstructionGraphTest
+    {
+        private static readonly Func<int> _sut = () => 1 + 2;
+        private static readonly Func<int> _branchedSut = () => _sut != null ? 1 : 2;
+
+        private static readonly Action _looper = () =>
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+        };
+
+        [Test]
+        public void ItShouldBePossibleToCreateFromMethod()
+        {
+            var method = _sut.AsMethodDef();
+            var igraph = InstructionGraph.CreateFrom(method);
+            Assert.AreEqual(5, igraph.Order);
+        }
+
+        [Test]
+        public void ItShouldHaveASinglePathWhenThereIsNoBranching()
+        {
+            var method = _sut.AsMethodDef();
+            var igraph = InstructionGraph.CreateFrom(method);
+            var paths = igraph.FindInstructionPaths();
+            Assert.AreEqual(1, paths.Count());
+        }
+
+        [Test]
+        public void ItShouldHaveTwoPathsWhenThereIsABranchInstruction()
+        {
+            var method = _branchedSut.AsMethodDef();
+            var igraph = InstructionGraph.CreateFrom(method);
+            var paths = igraph.FindInstructionPaths();
+            Assert.AreEqual(2, paths.Count());
+        }
+
+        [Test]
+        public void ItShouldHaveTwoPathsWhenThereIsALoop()
+        {
+            var method = _looper.AsMethodDef();
+            var igraph = InstructionGraph.CreateFrom(method);
+            var paths = igraph.FindInstructionPaths();
+            Assert.AreEqual(2, paths.Count());
+        }
+
+        [Test]
+        public void ItShouldDetectALoopingPath()
+        {
+            var method = _looper.AsMethodDef();
+            var igraph = InstructionGraph.CreateFrom(method);
+            var paths = igraph.FindInstructionPaths();
+            Assert.True(paths.Any(p => p.ContainsLoop()));
+        }
+
+        [Test]
+        public void ItShouldNotReportANonLoopingPathAsLooping()
+        {
+            var method = _sut.AsMethodDef();
+            var igraph = InstructionGraph.CreateFrom(method);
+            var paths = igraph.FindInstructionPaths();
+            Assert.False(paths.Any(p => p.ContainsLoop()));
+        }
+    }
+}
---
+++ TestNess.Lib/Cil/InstructionGraph.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GraphBuilder;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace TestNess.Lib.Cil
+{
+    public static class InstructionGraph
+    {
+        public static Graph<Instruction> CreateFrom(MethodDefinition method)
+        {
+            return CreateInstructionGraph(method);
+        }
+
+        public static IEnumerable<IList<Instruction>> FindInstructionPaths(this Graph<Instruction> igraph)
+        {
+            var returns = igraph.Walk().Where(i => i.OpCode == OpCodes.Ret);
+            var paths = new List<IList<Instruction>>();
+            foreach (var ret in returns)
+            {
+                paths.AddRange(igraph.FindPaths(igraph.Root, ret));
+            }
+            return paths;
+        }
+
+        public static bool ContainsLoop(this IList<Instruction> path)
+        {
+            return path.Count != path.Distinct().Count();
+        }
+
+        private static Graph<Instruction> CreateInstructionGraph(MethodDefinition method)
+        {
+            var builder = new GraphBuilder<Instruction>(NextInstructions);
+            return builder.Build(method.Body.Instructions[0]);
+        }
+
+        private static IEnumerable<Instruction> NextInstructions(Instruction v)
+        {
+            var fc = v.OpCode.FlowControl;
+            switch (fc)
+            {
+                case FlowControl.Next:
+                case FlowControl.Call: // stay within the method
+                    yield return v.Next;
+                    break;
+
+                case FlowControl.Return:
+                    yield break;
+
+                case FlowControl.Cond_Branch:
+                    yield return v.Next;
+                    if (v.Operand is Instruction[])
+                    {
+                        // switch statement
+                        foreach (var i in (Instruction[])v.Operand)
+                            yield return i;
+                    }
+                    else
+                        yield return (Instruction)v.Operand;
+                    break;
+
+                case FlowControl.Branch:
+                    yield return (Instruction)v.Operand;
+                    break;
+
+                default:
+                    throw new NotImplementedException(fc.ToString());
+            }
+        }
+    }
+}
