e215b2f6afd2ebc21954d4b95bdb40f686ea1036
Per Rovegård <per@rovegard.se>
2014-09-19 09:18:34 +0200

    Added a shared assembly info file, set the version to 0.1.0

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+
+[assembly: AssemblyDescription("A tool for measuring test suite quality")]
+[assembly: AssemblyCompany("TestNess")]
+[assembly: AssemblyProduct("TestNess")]
+[assembly: AssemblyCopyright("Copyright (C) Per Rovegard")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
