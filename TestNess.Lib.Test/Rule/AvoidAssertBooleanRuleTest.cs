29e4958c23b4be0a836999a5acdefeb104b622ce
Per Rovegård <per@rovegard.se>
2014-12-13 11:31:33 +0100

    Added AvoidAssertBooleanRule
    This rule checks that a test case doesn't perform a boolean assertion of
    a composite boolean value, e.g. checks that x == y is true.

---
+++ TestNess.Lib.Test/Rule/AvoidAssertBooleanRuleTest.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (C) 2014 Per Rovegård, http://rovegard.com
+// This file is subject to the terms and conditions of the MIT license. See the file 'LICENSE',
+// which is part of this source code package, or http://per.mit-license.org/2014.
+using System;
+using System.Linq;
+using NUnit.Framework;
+using TestNess.Lib.Rule;
+using TestNess.Lib.TestFramework;
+
+namespace TestNess.Lib.Test.Rule
+{
+    [TestFixture]
+    public class AvoidAssertBooleanRuleTest
+    {
+        private static readonly Action _violatingAction = () =>
+        {
+            var x = 5;
+            var y = 6;
+            Assert.False(x == y);
+        };
+
+        [Test]
+        public void ItShouldNotComplainAboutAssertingASingleBooleanValue()
+        {
+            Action a = () =>
+            {
+                var actual = Environment.Is64BitOperatingSystem;
+                Assert.True(actual);
+            };
+            var rule = new AvoidAssertBooleanRule();
+            var violations = rule.Apply(a.AsTestCase(new NUnitTestFramework()));
+            Assert.AreEqual(0, violations.Count());
+        }
+
+        [Test]
+        public void ItShouldComplainAboutAssertingABooleanValueResultingFromAnOperatorComparison()
+        {
+            var rule = new AvoidAssertBooleanRule();
+            var violations = rule.Apply(_violatingAction.AsTestCase(new NUnitTestFramework()));
+            Assert.AreEqual(1, violations.Count());
+        }
+
+        [Test]
+        public void ItShouldProvideAMessage()
+        {
+            var rule = new AvoidAssertBooleanRule();
+            var violations = rule.Apply(_violatingAction.AsTestCase(new NUnitTestFramework()));
+            var message = violations.First().Message;
+            Assert.AreEqual("Assert.False performs a boolean test on a composite boolean value", message);
+        }
+
+        [Test]
+        public void ItShouldPointToTheLocationOfTheAssertion()
+        {
+            var rule = new AvoidAssertBooleanRule();
+            var violations = rule.Apply(_violatingAction.AsTestCase(new NUnitTestFramework()));
+            var location = violations.First().Location;
+            Assert.AreEqual(19, location.StartLine);
+        }
+
+        [Test]
+        public void ItShouldHaveAToStringDescription()
+        {
+            var rule = new AvoidAssertBooleanRule();
+            Assert.AreEqual("a test case should not do a boolean assertion of a composite boolean value", rule.ToString());
+        }
+
+        [Test]
+        public void ItShouldComplainAboutAssertingABooleanValueResultingFromAEqualsComparison()
+        {
+            Action a = () =>
+            {
+                var x = 5;
+                var y = 6;
+                Assert.False(x.Equals(y));
+            };
+            var rule = new AvoidAssertBooleanRule();
+            var violations = rule.Apply(a.AsTestCase(new NUnitTestFramework()));
+            Assert.AreEqual(1, violations.Count());
+        }
+    }
+}
---
+++ TestNess.Lib/Rule/AvoidAssertBooleanRule.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (C) 2014 Per Rovegård, http://rovegard.com
+// This file is subject to the terms and conditions of the MIT license. See the file 'LICENSE',
+// which is part of this source code package, or http://per.mit-license.org/2014.
+
+using System.Collections.Generic;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using TestNess.Lib.Cil;
+using TestNess.Lib.TestFramework;
+
+namespace TestNess.Lib.Rule
+{
+    public class AvoidAssertBooleanRule : IRule
+    {
+        public IEnumerable<Violation> Apply(TestCase testCase)
+        {
+            var calledAssertingMethods = testCase.GetCalledAssertingMethods();
+            var tracker = new MethodValueTracker(testCase.TestMethod);
+
+            // For each asserting method with >= 1 parameters:
+            foreach (var cm in calledAssertingMethods)
+            {
+                var methodRef = cm.MethodReference;
+                var parameterPurposes = testCase.Framework.GetParameterPurposes(methodRef);
+                if (!IsSingleTruthCheckingMethod(methodRef, parameterPurposes))
+                    continue;
+
+                foreach (var valueGraph in tracker.ValueGraphs)
+                {
+                    IList<MethodValueTracker.Value> consumedValues =
+                        tracker.GetConsumedValues(valueGraph, cm.Instruction).ToList();
+                    if (consumedValues.Count == 0)
+                        continue; // not part of value graph
+                    var interestingValue = consumedValues[0];
+                    var producers = UltimateProducers(interestingValue);
+                    if (producers.Count > 1)
+                    {
+                        yield return new Violation(this, testCase, cm.Instruction, string.Format("{0}.{1} performs a boolean test on a composite boolean value",
+                            cm.MethodReference.DeclaringType.Name,
+                            cm.MethodReference.Name));
+                    }
+                }
+            }
+        }
+
+        private ICollection<Instruction> UltimateProducers(MethodValueTracker.Value v)
+        {
+            // We track backwards and record values without parents. The producers of
+            // those values are considered the ultimate producers of the given value.
+            var result = new List<Instruction>();
+            var values = new Queue<MethodValueTracker.Value>();
+            values.Enqueue(v);
+            while (values.Count > 0)
+            {
+                var aValue = values.Dequeue();
+                if (aValue.Parents.Count == 0)
+                {
+                    result.Add(aValue.Producer);
+                    continue;
+                }
+                foreach (var parent in aValue.Parents)
+                {
+                    values.Enqueue(parent);
+                }
+            }
+            return result;
+        }
+
+        // TODO: Copied (almost) from LocalExpectationRule
+        private static bool IsSingleTruthCheckingMethod(MethodReference method, IList<ParameterPurpose> purposes)
+        {
+            var reduced = method.ReduceToShortestOverload();
+            return reduced.Parameters.Count == 1 && method.Parameters[0].ParameterType.FullName == "System.Boolean" &&
+                   purposes[0] == ParameterPurpose.Actual;
+        }
+
+        public override string ToString()
+        {
+            return "a test case should not do a boolean assertion of a composite boolean value";
+        }
+    }
+}
